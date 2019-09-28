e4b9d699e4b140e1fd393d0f7a1577236b714f55
dyang504 <dengyang92@qq.com>
2019-09-28 21:36:43 +0800

    create the website

---
+++ content/pages/about.md
@@ -0,0 +1,9 @@
+title: About
+
+I'm Ian, a data analyst, inovator.
+
+
+You can find me on:
+[Twitter](https://twitter/)
+[Github]()
+[Kaggle]()
---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'ian'
+SITENAME = 'Logfok'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Chongqing'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
