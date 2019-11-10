a1debb5defef4e8a95bcd0cfec847028fe44647e
dyang504 <dengyang92@qq.com>
2019-11-10 15:45:07 +0800

    make source branch

---
+++ content/pages/projects.md
@@ -0,0 +1,10 @@
+title: Projects
+
+
+Here are some of my data science project.
+
+2019
+
+1. [Predict sales using monte calro simulation](https://github.com/dyang504/Monte_calro_prediction)
+
+2. [dashboard](https://github.com/dyang504/workout_dashboard)
---
+++ pelicanconf.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'ian'
+SITENAME = 'Logfok'
+SITEURL = 'https://dyang504.github.io/site'
+
+
+PATH = 'content'
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['neighbors']
+
+THEME = 'theme_yellow'
+THEME_STATIC_DIR = 'static'
+THEME_STATIC_PATHS = ['static']
+CSS_FILE = 'main.css'
+
+TIMEZONE = 'Asia/Chongqing'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+GOOGLE_ANALYTICS = "UA-134156222-1"
+
+GOOGLE_ANALYTICS_UNIVERSAL_PROPERTY = 'auto'
+
+STATIC_PATHS=[
+    'images',
+    'extra',
+]
+
+EXTRA_PATH_METADATA = {
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/CNAME':{'path':'CNAME'},
+}
